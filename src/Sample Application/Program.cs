f62f7478a3f0286fa664e9b0a237578fd11a8964
nsrinath <irtmail-github@yahoo.in>
2013-11-11 19:08:50 -0600

    Submitting sample app on behalf of Raj
    Submitting sample app on behalf of Raj

---
+++ src/Sample Application/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo info = new ProcessStartInfo("ffmpeg.exe");
+            info.Arguments = " -i " + "Wildlife.wmv" + " -vn -ac 2 -ar 44100 -ab 320k -f mp3 " + "output.mp3";
+
+            Process.Start(info);
+            Console.ReadKey();
+        }
+    }
+}
