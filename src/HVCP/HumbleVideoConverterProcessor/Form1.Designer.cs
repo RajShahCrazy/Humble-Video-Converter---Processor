9fc053b0b4db8a1f5999b6ff10e24474aaf7a0d7
jwookyoo <knighterrant11@gmail.com>
2013-11-13 19:29:44 -0600

    Create the subtitle form and link it to the form 1

--- src/HVCP/HumbleVideoConverterProcessor/Form1.Designer.cs
+++ src/HVCP/HumbleVideoConverterProcessor/Form1.Designer.cs
@@ -72,7 +72,8 @@
             this.Controls.Add(this.ProcessButton);
             this.Controls.Add(this.ConvertButton);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "HVCP - Main";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- src/HVCP/HumbleVideoConverterProcessor/Form1.cs
+++ src/HVCP/HumbleVideoConverterProcessor/Form1.cs
@@ -9,11 +9,15 @@ using System.Windows.Forms;
 
 namespace HumbleVideoConverterProcessor
 {
+
     public partial class Form1 : Form
     {
+        private Form2 form_subtitle;
+
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void ConvertButton_Click(object sender, EventArgs e)
@@ -27,6 +31,13 @@ namespace HumbleVideoConverterProcessor
         }
 
         private void SubtitlesButton_Click(object sender, EventArgs e)
+        {
+            form_subtitle = new Form2();
+            form_subtitle.Activate();
+            form_subtitle.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
