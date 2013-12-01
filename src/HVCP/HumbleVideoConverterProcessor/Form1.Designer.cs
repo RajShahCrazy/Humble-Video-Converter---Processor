e711ac6ae113384526b787d29a8770199a287682
jwookyoo <knighterrant11@gmail.com>
2013-11-30 21:17:55 -0600

    Form 1 - change the design of the first window

--- src/HVCP/HumbleVideoConverterProcessor/Form1.Designer.cs
+++ src/HVCP/HumbleVideoConverterProcessor/Form1.Designer.cs
@@ -42,38 +42,40 @@
             this.convertToolTip = new System.Windows.Forms.ToolTip(this.components);
             this.processTooltip = new System.Windows.Forms.ToolTip(this.components);
             this.subtitlesTooltip = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ConvertButton
             // 
-            this.ConvertButton.Location = new System.Drawing.Point(42, 33);
+            this.ConvertButton.Location = new System.Drawing.Point(37, 106);
             this.ConvertButton.Name = "ConvertButton";
-            this.ConvertButton.Size = new System.Drawing.Size(75, 23);
+            this.ConvertButton.Size = new System.Drawing.Size(249, 23);
             this.ConvertButton.TabIndex = 0;
-            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.Text = "Video Format Converter";
             this.convertToolTip.SetToolTip(this.ConvertButton, "Convert video from one format to another");
             this.ConvertButton.UseVisualStyleBackColor = true;
             this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
             // 
             // ProcessButton
             // 
-            this.ProcessButton.Location = new System.Drawing.Point(122, 115);
+            this.ProcessButton.Location = new System.Drawing.Point(37, 135);
             this.ProcessButton.Name = "ProcessButton";
-            this.ProcessButton.Size = new System.Drawing.Size(75, 23);
+            this.ProcessButton.Size = new System.Drawing.Size(249, 23);
             this.ProcessButton.TabIndex = 1;
-            this.ProcessButton.Text = "Process";
+            this.ProcessButton.Text = "Video Processor";
             this.processTooltip.SetToolTip(this.ProcessButton, "Process videos by adding effects");
             this.ProcessButton.UseVisualStyleBackColor = true;
             this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
             // 
             // SubtitlesButton
             // 
-            this.SubtitlesButton.Location = new System.Drawing.Point(216, 189);
+            this.SubtitlesButton.Location = new System.Drawing.Point(37, 164);
             this.SubtitlesButton.Name = "SubtitlesButton";
-            this.SubtitlesButton.Size = new System.Drawing.Size(75, 23);
+            this.SubtitlesButton.Size = new System.Drawing.Size(249, 23);
             this.SubtitlesButton.TabIndex = 2;
-            this.SubtitlesButton.Text = "Subtitles";
+            this.SubtitlesButton.Text = "Subtitle Editor";
             this.subtitlesTooltip.SetToolTip(this.SubtitlesButton, "Add subtitles to a video");
             this.SubtitlesButton.UseVisualStyleBackColor = true;
             this.SubtitlesButton.Click += new System.EventHandler(this.SubtitlesButton_Click);
@@ -88,7 +90,7 @@
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(343, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(336, 24);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -104,7 +106,7 @@
             // 
             this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
             this.exitToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(134, 22);
             this.exitToolStripMenuItem1.Text = "Exit";
             this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
             // 
@@ -142,11 +144,33 @@
             // 
             this.convertToolTip.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Welcome to";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Humble Video Converter Processor";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(343, 257);
+            this.ClientSize = new System.Drawing.Size(336, 200);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.SubtitlesButton);
             this.Controls.Add(this.ProcessButton);
@@ -176,6 +200,8 @@
         private System.Windows.Forms.ToolTip convertToolTip;
         private System.Windows.Forms.ToolTip processTooltip;
         private System.Windows.Forms.ToolTip subtitlesTooltip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
